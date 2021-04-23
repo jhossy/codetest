320efcf68c0c8c54500ec153d1ed861b7976e5d3
Jesper Hossy <jhossy@hotmail.com>
2021-04-23 20:30:01 +0200

    initialæ project setup inc tests

---
+++ Src/CodeTest/Controllers/NumeralsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTest.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumeralsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult NumeralsToDigits()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
---
+++ Src/CodeTest/Infrastructure/RomansConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTest.Web.Infrastructure
+{
+    public interface IRomansConverter
+    {
+        int FromNumeral(string numeral);
+    }
+
+    public class RomansConverter : IRomansConverter
+    {
+        private readonly Dictionary<char, int> _numeralValues = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public RomansConverter()
+        {
+            
+        }
+
+        public int FromNumeral(string numeral)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
