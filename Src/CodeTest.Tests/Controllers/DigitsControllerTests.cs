7f3ec60e34f7afb333e1e7f6fc4d9c2999757e89
Jesper Hossy <jhossy@hotmail.com>
2021-04-25 19:33:54 +0200

    added basic controller tests

---
+++ Src/CodeTest.Tests/Controllers/DigitsControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using CodeTest.Web.Controllers;
+using CodeTest.Web.Infrastructure;
+using Moq;
+using System;
+using Xunit;
+
+namespace CodeTest.Tests.Controllers
+{
+    public class DigitsControllerTests
+    {
+        [Fact]
+        public void ItShouldConstruct()
+        {
+            //Arrange
+            Mock<IDigitsValidator> mockDigitsValidator = new Mock<IDigitsValidator>();
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+
+            DigitsController sut = new DigitsController(mockDigitsValidator.Object, mockRomansConverter.Object);
+
+            //Act
+
+            //Assert
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void ItShouldThrow()
+        {
+            //Arrange
+            Mock<IDigitsValidator> mockDigitsValidator = new Mock<IDigitsValidator>();
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+            
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new DigitsController(null, mockRomansConverter.Object));
+            Assert.Throws<ArgumentNullException>(() => new DigitsController(mockDigitsValidator.Object, null));
+        }
+    }
+}
---
+++ Src/CodeTest.Tests/Controllers/NumeralsControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using CodeTest.Web.Controllers;
+using CodeTest.Web.Infrastructure;
+using Moq;
+using System;
+using Xunit;
+
+namespace CodeTest.Tests.Controllers
+{
+    public class NumeralsControllerTests
+    {
+        [Fact]
+        public void ItShouldConstruct()
+        {
+            //Arrange
+            Mock<INumeralsValidator> mockNumeralsValidator = new Mock<INumeralsValidator>();
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+
+            NumeralsController sut = new NumeralsController(mockNumeralsValidator.Object, mockRomansConverter.Object);
+
+            //Act
+
+            //Assert
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void ItShouldThrow()
+        {
+            //Arrange
+            Mock<INumeralsValidator> mockNumeralsValidator = new Mock<INumeralsValidator>();
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new NumeralsController(null, mockRomansConverter.Object));
+            Assert.Throws<ArgumentNullException>(() => new NumeralsController(mockNumeralsValidator.Object, null));
+        }
+    }
+}
