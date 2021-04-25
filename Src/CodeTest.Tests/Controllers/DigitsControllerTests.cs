29649da389495e0eae2d64c2ab1debda9dfae0bb
Jesper Hossy <jhossy@hotmail.com>
2021-04-25 19:42:22 +0200

    added missing controller tests

--- Src/CodeTest.Tests/Controllers/DigitsControllerTests.cs
+++ Src/CodeTest.Tests/Controllers/DigitsControllerTests.cs
@@ -1,5 +1,7 @@
-﻿using CodeTest.Web.Controllers;
+﻿using AutoFixture.Xunit2;
+using CodeTest.Web.Controllers;
 using CodeTest.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using Xunit;
@@ -35,6 +37,46 @@ namespace CodeTest.Tests.Controllers
             //Assert
             Assert.Throws<ArgumentNullException>(() => new DigitsController(null, mockRomansConverter.Object));
             Assert.Throws<ArgumentNullException>(() => new DigitsController(mockDigitsValidator.Object, null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ItShouldReturnBadRequest(int input)
+        {
+            //Arrange
+            Mock<IDigitsValidator> mockDigitsValidator = new Mock<IDigitsValidator>();
+            mockDigitsValidator.Setup(mockDigitsValidator => mockDigitsValidator.Validate(input))
+                .Returns(false);
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+
+            DigitsController sut = new DigitsController(mockDigitsValidator.Object, mockRomansConverter.Object);
+
+            //Act
+            IActionResult result = sut.DigitToNumerals(input);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Theory]
+        [AutoData]
+        public void ItShouldReturnOkResult(int input)
+        {
+            //Arrange
+            Mock<IDigitsValidator> mockDigitsValidator = new Mock<IDigitsValidator>();
+            mockDigitsValidator.Setup(mockDigitsValidator => mockDigitsValidator.Validate(input))
+                .Returns(true);
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+            mockRomansConverter.Setup(mockRomansConverter => mockRomansConverter.ToNumeral(input))
+                .Returns("I");
+
+            DigitsController sut = new DigitsController(mockDigitsValidator.Object, mockRomansConverter.Object);
+
+            //Act
+            IActionResult result = sut.DigitToNumerals(input);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
--- Src/CodeTest.Tests/Controllers/NumeralsControllerTests.cs
+++ Src/CodeTest.Tests/Controllers/NumeralsControllerTests.cs
@@ -1,5 +1,7 @@
-﻿using CodeTest.Web.Controllers;
+﻿using AutoFixture.Xunit2;
+using CodeTest.Web.Controllers;
 using CodeTest.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using Xunit;
@@ -35,6 +37,46 @@ namespace CodeTest.Tests.Controllers
             //Assert
             Assert.Throws<ArgumentNullException>(() => new NumeralsController(null, mockRomansConverter.Object));
             Assert.Throws<ArgumentNullException>(() => new NumeralsController(mockNumeralsValidator.Object, null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ItShouldReturnBadRequest(string input)
+        {
+            //Arrange
+            Mock<INumeralsValidator> mockNumeralsValidator = new Mock<INumeralsValidator>();
+            mockNumeralsValidator.Setup(mockNumeralsValidator => mockNumeralsValidator.Validate(input))
+                .Returns(false);
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+
+            NumeralsController sut = new NumeralsController(mockNumeralsValidator.Object, mockRomansConverter.Object);
+
+            //Act
+            IActionResult result = sut.NumeralsToDigits(input);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Theory]
+        [AutoData]
+        public void ItShouldReturnOkResult(string input)
+        {
+            //Arrange
+            Mock<INumeralsValidator> mockNumeralsValidator = new Mock<INumeralsValidator>();
+            mockNumeralsValidator.Setup(mockNumeralsValidator => mockNumeralsValidator.Validate(input))
+                .Returns(true);
+            Mock<IRomansConverter> mockRomansConverter = new Mock<IRomansConverter>();
+            mockRomansConverter.Setup(mockRomansConverter => mockRomansConverter.FromNumeral(input))
+                .Returns(1);
+
+            NumeralsController sut = new NumeralsController(mockNumeralsValidator.Object, mockRomansConverter.Object);
+
+            //Act
+            IActionResult result = sut.NumeralsToDigits(input);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
